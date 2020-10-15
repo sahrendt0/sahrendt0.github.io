6e28edd56a222b978b0433c64c50ae2c26e2ac95
Steven Ahrendt <sahrendt0@gmail.com>
2020-10-15 11:40:35 -0700

    Try for chords

---
+++ Batpra1-mitochondrion.links.js
@@ -0,0 +1,17 @@
+var CHORD1 = [ "CHORD1" , {
+			CHORDinnerRadius: 175,
+			CHORDouterRadius: 180,
+			CHORDFillOpacity:0.67,
+			CHORDStrokeColor: "black",
+			CHORDStrokeWidth: "1px",
+			CHORDPadding:0,
+			CHORDAutoFillColor: false,
+			CHORDFillColor:["red","blue","green"],
+			} , [
+			[ " " , " "],
+			[
+			  [1, 1],
+			  [1, 1],
+			]
+		]];
+		
